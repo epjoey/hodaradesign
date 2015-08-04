17bc54aa48912d5a3d2d02ad9d894b21a3e1171f
epjoey <joey@kanjoya.com>
2015-08-03 23:26:39 -0700

    noriegatree

--- fineart/index.php
+++ fineart/index.php
@@ -3,6 +3,7 @@
    <div id="sec-head5"><a href="/fineart"><img src="../port/fine1.gif"></a></div>
 
 <div id="thumbs31">
+<a href="?swirly"><img src="thumbs/x-swirlysunset.jpg" border="0"></a><br/><br/> 
 <a href="?noriega"><img src="thumbs/x-noriega.jpg" border="0"></a><br/><br/> 
 <a href="?ob"><img src="thumbs/x-ob.jpg" border="0"></a><br/><br/> 
 <a href="?backside"><img src="thumbs/x-backside.jpg" border="0"></a><br/><br/> 
@@ -39,6 +40,8 @@
 									<p>I enjoy sketching, painting and printmaking. My subject matter includes primarily portraiture and surf art.</p>
 								<p class='small'>&larr; click on those thumbnails to view work</p>";
 									}
+								elseif (isset($_GET['swirly'])){
+			echo "<h2>Sunset Ghost</h2><p></p><img src=images/swirlysunset.jpg /><br/><p class='small'>Tools used: Linoleum cut on rice paper</p>";									}			
 								elseif (isset($_GET['noriega'])){
 			echo "<h2>Noriega Tree</h2><p></p><img src=images/noriega.jpg /><br/><p class='small'>Tools used: Linoleum cut on rice paper</p>";									}			
 								elseif (isset($_GET['ob'])){
