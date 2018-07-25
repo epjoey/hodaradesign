4b56dd2de9dc6ade8b63e580c83deaaef3f294bd
Joey <joey@fareharbor.com>
2018-07-25 00:22:06 -1000

    Cleanup

---
+++ checkout/index.php
@@ -0,0 +1,4 @@
+<?php include '../header.php'; ?>
+
+<script src="https://unpkg.com/react-stripe-elements@latest/dist/react-stripe-elements.min.js"></script>
+<script src="https://js.stripe.com/v3/"></script>
