794a2c0b9955a787f9a3cc2c3ee487468a2efe18
Joey <joey@fareharbor.com>
2018-08-01 11:03:10 -1000

    Cleanup

--- header.php
+++ header.php
@@ -66,7 +66,18 @@
 
 
         <div id="header">
-            <?php include 'header-top.php' ?>
+            <div id="tophead">
+                <div id="topnav">
+                    <a <?php if($section == 'port') echo 'class="on"' ?> id="firsttop" href="/port">portfolio</a>
+                    <a <?php if($section == 'about') echo 'class="on"' ?> id="sectop" href="/about">about</a>
+                    <a <?php if($section == 'contact') echo 'class="on"' ?> id="lasttop" href="/contact">contact</a>
+                </div>
+
+                <div id="email">
+                    <a href="mailto:jhodara@gmail.com" title="You sure you want to open your e-mail program?">jhodara@gmail.com</a>
+                </div>
+            </div>
+
             <div id="nav">
 
                 <div id="nleft"><img alt="" src="<?php $base ?>/graphics/row5.1.jpg" width="121"></div>
--- index.php
+++ index.php
@@ -8,15 +8,12 @@
     <meta name="Keywords" content="design, hodara, joey, joey hodara, joseph hodara, graphic design, web design, AIGA, golden league, dawn patrol ">
     <link rel="stylesheet" type="text/css" media="all" href="<?php $base ?>/css/store.css" />
     <link rel="icon" type="image/gif" href="<?php $base ?>/favicon.ico" />
-    <script type="application/javascript" src="https://unpkg.com/react@16.0.0/umd/react.production.min.js"></script>
-    <script type="application/javascript" src="https://unpkg.com/react-dom@16.0.0/umd/react-dom.production.min.js"></script>
-    <script type="application/javascript" src="https://unpkg.com/babel-standalone@6.26.0/babel.js"></script>
   </head>
   <body>
     <div id="store-container">
       <div id="store-root"></div>
 
-      <script type="application/javascript" src="<?php $base ?>/js/index.js"></script>
+      <script type="application/javascript" src="<?php $base ?>/dist/bundle.js"></script>
 
       <div id="store-left"><span></span></div>
       <div id="store-right"><span></span></div>
