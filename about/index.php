2196544feef4c8fedd65aa7bebb775ec3ea220f3
Joey <joey@fareharbor.com>
2018-09-19 16:03:33 -0700

    Resume

--- about/index.php
+++ about/index.php
@@ -2,7 +2,7 @@
 <div id="ac">
 	<img src="../images/grad.jpg">
 	<br />
-	<p><i>Name:</i> Joseph Hodara &nbsp <i>From:</i> Upcountry, Maui &nbsp  <i>Now:</i> San Francisco</p>
+	<p><i>Name:</i> Joseph Hodara &nbsp <i>From:</i> Upcountry, Maui &nbsp  <i>Now:</i> Santa Cruz, CA</p>
 	<p>Hodara Design is the graphic design and fine art portfolio of Joseph (Joey) Hodara.</p>
 	<p><i>Mahalo</i></p>
 	<p class="resume"><a href="/about/Hodara_Resume.pdf">Download Resume</a></p>
