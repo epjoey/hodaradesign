c7920d404c93648c373f29afb30fae16497d83f6
joey hodara <jhodara@gmail.com>
2025-11-16 17:15:08 -1000

    fix webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,41 @@
 const path = require('path');
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: path.resolve(__dirname, 'src', 'index.js'),
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
+    filename: 'bundle.js',
+    clean: true,
   },
+  devServer: {
+    historyApiFallback: true, // needed for SPA routing
+    hot: true,
+    port: 8080,
+    static: {
+      directory: path.join(__dirname, 'public'),
+    },    
+  },  
+  resolve: {
+    extensions: ['.js', '.jsx'],
+    fallback: {
+      crypto: require.resolve('crypto-browserify'),
+      stream: require.resolve('stream-browserify'),
+      buffer: require.resolve('buffer/'),
+      path: require.resolve('path-browserify'),
+      process: require.resolve('process/browser'),
+    },
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      Buffer: ['buffer', 'Buffer'],
+      process: 'process/browser',
+    }),
+    new HtmlWebpackPlugin({
+      template: './public/index.html', // source HTML
+    })    
+  ],
   module: {
     rules: [
       {
